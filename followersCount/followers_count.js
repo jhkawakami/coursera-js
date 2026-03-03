d3a0abce0a61e5f772884452ae1f6058e63f50ea
jhkawakami <jhkawakami@gmail.com>
2026-03-03 14:38:04 -0800

    Made sample website and javascript to demonstrate function closures

---
+++ followersCount/followers_count.js
@@ -0,0 +1,21 @@
+let count = 0; // Initializing count to 0
+
+function increaseCount(){
+    count++; // Increase count by 1
+
+    displayCount(); // Display the count
+    checkCountValue(); // check out value and display messages if it hits milestone
+}
+
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML = count; // Display the count in the span with id 'countDisplay'
+}
+
+function checkCountValue(){
+    if (count === 10){
+        alert("You gained 10 followers! Congratulations!");
+    }
+    else if (count === 20){
+        alert("You gained 20 followers! Congratulations!");
+    }
+}
